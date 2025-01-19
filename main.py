4d452112fae1bdc8ba0c990566c0b5f9cb0a770c
PabloNvnes <pablonunes1500@gmail.com>
2025-01-19 03:45:17 -0300

    teste

--- main.py
+++ main.py
@@ -20,6 +20,89 @@ def get_solar_irradiance(lat, lon, date_time):
         irradiance = 0
     return irradiance
 
+def irradiancia():
+
+    # Lista de cidades representativas de cada estado do Brasil com suas coordenadas
+    cities = [
+        {"state": "São Paulo", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
+        {"state": "Rio de Janeiro", "lat": -22.9068, "lon": -43.1729, "timezone": "America/Sao_Paulo"},
+        {"state": "Minas Gerais", "lat": -19.9167, "lon": -43.9345, "timezone": "America/Sao_Paulo"},
+        {"state": "Distrito Federal", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
+        {"state": "Bahia", "lat": -12.9714, "lon": -38.5014, "timezone": "America/Bahia"},
+        {"state": "Ceará", "lat": -3.7172, "lon": -38.5434, "timezone": "America/Fortaleza"},
+        {"state": "Paraná", "lat": -25.4284, "lon": -49.2733, "timezone": "America/Sao_Paulo"},
+        {"state": "Amazonas", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"},
+        {"state": "Pernambuco", "lat": -8.0476, "lon": -34.8770, "timezone": "America/Recife"},
+        {"state": "Rio Grande do Sul", "lat": -30.0346, "lon": -51.2177, "timezone": "America/Sao_Paulo"},
+        {"state": "Pará", "lat": -1.4558, "lon": -48.4902, "timezone": "America/Belem"},
+        {"state": "Goiás", "lat": -16.6869, "lon": -49.2648, "timezone": "America/Sao_Paulo"},
+        {"state": "Maranhão", "lat": -2.5307, "lon": -44.3068, "timezone": "America/Fortaleza"},
+        {"state": "Alagoas", "lat": -9.6658, "lon": -35.7350, "timezone": "America/Maceio"},
+        {"state": "Rio Grande do Norte", "lat": -5.7945, "lon": -35.2110, "timezone": "America/Fortaleza"},
+        {"state": "Piauí", "lat": -5.0919, "lon": -42.8034, "timezone": "America/Fortaleza"},
+        {"state": "Paraíba", "lat": -7.1195, "lon": -34.8450, "timezone": "America/Fortaleza"},
+        {"state": "Sergipe", "lat": -10.9472, "lon": -37.0731, "timezone": "America/Maceio"},
+        {"state": "Mato Grosso do Sul", "lat": -20.4697, "lon": -54.6201, "timezone": "America/Campo_Grande"},
+        {"state": "Mato Grosso", "lat": -15.6014, "lon": -56.0979, "timezone": "America/Cuiaba"},
+        {"state": "Tocantins", "lat": -10.2491, "lon": -48.3243, "timezone": "America/Araguaina"},
+        {"state": "Roraima", "lat": 2.8235, "lon": -60.6758, "timezone": "America/Boa_Vista"},
+        {"state": "Amapá", "lat": 0.0340, "lon": -51.0694, "timezone": "America/Belem"},
+        {"state": "Rondônia", "lat": -8.7612, "lon": -63.9039, "timezone": "America/Porto_Velho"},
+        {"state": "Acre", "lat": -9.9747, "lon": -67.8243, "timezone": "America/Rio_Branco"}
+    ]
+
+    # Obter dados de irradiância solar para as últimas 24 horas para cada cidade
+    irradiance_data = {city["state"]: [] for city in cities}
+    time_data = []
+    current_time = datetime.now(pytz.utc)
+    for hour in range(24):
+        time_point = current_time - timedelta(hours=hour)
+        time_data.append(time_point)
+        for city in cities:
+            timezone = pytz.timezone(city["timezone"])
+            local_time = time_point.astimezone(timezone)
+            irradiance = get_solar_irradiance(city["lat"], city["lon"], local_time)
+            irradiance_data[city["state"]].append(irradiance)
+
+    # Calcular a irradiância solar média para cada estado
+    average_irradiance = {state: sum(irradiance_data[state]) / len(irradiance_data[state]) for state in irradiance_data}
+
+    # Criar DataFrame para facilitar a manipulação dos dados
+    df = pd.DataFrame(irradiance_data, index=time_data)
+
+    # Inverter os dados para que o tempo esteja em ordem crescente
+    df = df.iloc[::-1]
+
+    # Criar o gráfico com Plotly Express
+    fig = px.line(df, x=df.index, y=df.columns, labels={'value': 'Irradiância Solar (W/m²)', 'index': 'Hora'}, title='Irradiância Solar nas Últimas 24 Horas')
+    fig.update_layout(xaxis_title='Hora', yaxis_title='Irradiância Solar (W/m²)', legend_title_text='Estado')
+
+    # Exibir o gráfico no Streamlit
+    st.title('Gráfico de Irradiância Solar nas Últimas 24 Horas')
+    st.plotly_chart(fig)
+
+    # Exibir top 10 de maiores e menores irradiâncias médias
+    sorted_irradiance = sorted(average_irradiance.items(), key=lambda x: x[1], reverse=True)
+    top_10_highest = sorted_irradiance[:10]
+    top_10_lowest = sorted_irradiance[-10:]
+
+    # Criar DataFrames para os top 10
+    df_top_10_highest = pd.DataFrame(top_10_highest, columns=['Estado', 'Irradiância Média (W/m²)'])
+    df_top_10_lowest = pd.DataFrame(top_10_lowest, columns=['Estado', 'Irradiância Média (W/m²)']).iloc[::-1]
+
+    # Exibir tabelas estilizadas no Streamlit
+    st.title('Top 10 Maiores Irradiâncias Médias')
+    st.table(df_top_10_highest.style.format({'Irradiância Média (W/m²)': '{:.2f}'}))
+
+    st.title('Top 10 Menores Irradiâncias Médias')
+    st.table(df_top_10_lowest.style.format({'Irradiância Média (W/m²)': '{:.2f}'}))
+
+def performance():
+    return
+
+def disponibilidade():
+    return
+
 #####################
 ## Streamlit Geral ##
 #####################
@@ -31,77 +114,25 @@ add_sidebar = st.sidebar.selectbox('Análises', ('Irradiância Solar', 'Performa
 ## Irradiância ##
 #################
 
-# Lista de cidades representativas de cada estado do Brasil com suas coordenadas
-cities = [
-    {"state": "São Paulo", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
-    {"state": "Rio de Janeiro", "lat": -22.9068, "lon": -43.1729, "timezone": "America/Sao_Paulo"},
-    {"state": "Minas Gerais", "lat": -19.9167, "lon": -43.9345, "timezone": "America/Sao_Paulo"},
-    {"state": "Distrito Federal", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
-    {"state": "Bahia", "lat": -12.9714, "lon": -38.5014, "timezone": "America/Bahia"},
-    {"state": "Ceará", "lat": -3.7172, "lon": -38.5434, "timezone": "America/Fortaleza"},
-    {"state": "Paraná", "lat": -25.4284, "lon": -49.2733, "timezone": "America/Sao_Paulo"},
-    {"state": "Amazonas", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"},
-    {"state": "Pernambuco", "lat": -8.0476, "lon": -34.8770, "timezone": "America/Recife"},
-    {"state": "Rio Grande do Sul", "lat": -30.0346, "lon": -51.2177, "timezone": "America/Sao_Paulo"},
-    {"state": "Pará", "lat": -1.4558, "lon": -48.4902, "timezone": "America/Belem"},
-    {"state": "Goiás", "lat": -16.6869, "lon": -49.2648, "timezone": "America/Sao_Paulo"},
-    {"state": "Maranhão", "lat": -2.5307, "lon": -44.3068, "timezone": "America/Fortaleza"},
-    {"state": "Alagoas", "lat": -9.6658, "lon": -35.7350, "timezone": "America/Maceio"},
-    {"state": "Rio Grande do Norte", "lat": -5.7945, "lon": -35.2110, "timezone": "America/Fortaleza"},
-    {"state": "Piauí", "lat": -5.0919, "lon": -42.8034, "timezone": "America/Fortaleza"},
-    {"state": "Paraíba", "lat": -7.1195, "lon": -34.8450, "timezone": "America/Fortaleza"},
-    {"state": "Sergipe", "lat": -10.9472, "lon": -37.0731, "timezone": "America/Maceio"},
-    {"state": "Mato Grosso do Sul", "lat": -20.4697, "lon": -54.6201, "timezone": "America/Campo_Grande"},
-    {"state": "Mato Grosso", "lat": -15.6014, "lon": -56.0979, "timezone": "America/Cuiaba"},
-    {"state": "Tocantins", "lat": -10.2491, "lon": -48.3243, "timezone": "America/Araguaina"},
-    {"state": "Roraima", "lat": 2.8235, "lon": -60.6758, "timezone": "America/Boa_Vista"},
-    {"state": "Amapá", "lat": 0.0340, "lon": -51.0694, "timezone": "America/Belem"},
-    {"state": "Rondônia", "lat": -8.7612, "lon": -63.9039, "timezone": "America/Porto_Velho"},
-    {"state": "Acre", "lat": -9.9747, "lon": -67.8243, "timezone": "America/Rio_Branco"}
-]
+if add_sidebar == 'Irradiância Solar':
+    st.title('Irradiância Solar')
 
-# Obter dados de irradiância solar para as últimas 24 horas para cada cidade
-irradiance_data = {city["state"]: [] for city in cities}
-time_data = []
-current_time = datetime.now(pytz.utc)
-for hour in range(24):
-    time_point = current_time - timedelta(hours=hour)
-    time_data.append(time_point)
-    for city in cities:
-        timezone = pytz.timezone(city["timezone"])
-        local_time = time_point.astimezone(timezone)
-        irradiance = get_solar_irradiance(city["lat"], city["lon"], local_time)
-        irradiance_data[city["state"]].append(irradiance)
+    irradiancia()
 
-# Calcular a irradiância solar média para cada estado
-average_irradiance = {state: sum(irradiance_data[state]) / len(irradiance_data[state]) for state in irradiance_data}
+#############################
+## Performance dos Estados ##
+#############################
 
-# Criar DataFrame para facilitar a manipulação dos dados
-df = pd.DataFrame(irradiance_data, index=time_data)
+if add_sidebar == 'Performance dos Estados':
+    st.write('Performance dos Estados')
 
-# Inverter os dados para que o tempo esteja em ordem crescente
-df = df.iloc[::-1]
+    performance()
 
-# Criar o gráfico com Plotly Express
-fig = px.line(df, x=df.index, y=df.columns, labels={'value': 'Irradiância Solar (W/m²)', 'index': 'Hora'}, title='Irradiância Solar nas Últimas 24 Horas')
-fig.update_layout(xaxis_title='Hora', yaxis_title='Irradiância Solar (W/m²)', legend_title_text='Estado')
+################################
+## Disponibilidade de Energia ##
+################################
 
-# Exibir o gráfico no Streamlit
-st.title('Gráfico de Irradiância Solar nas Últimas 24 Horas')
-st.plotly_chart(fig)
+if add_sidebar == 'Disponibilidade de Energia':
+    st.write('Disponibilidade de Energia')
 
-# Exibir top 10 de maiores e menores irradiâncias médias
-sorted_irradiance = sorted(average_irradiance.items(), key=lambda x: x[1], reverse=True)
-top_10_highest = sorted_irradiance[:10]
-top_10_lowest = sorted_irradiance[-10:]
-
-# Criar DataFrames para os top 10
-df_top_10_highest = pd.DataFrame(top_10_highest, columns=['Estado', 'Irradiância Média (W/m²)'])
-df_top_10_lowest = pd.DataFrame(top_10_lowest, columns=['Estado', 'Irradiância Média (W/m²)'])
-
-# Exibir tabelas estilizadas no Streamlit
-st.title('Top 10 Maiores Irradiâncias Médias')
-st.table(df_top_10_highest.style.format({'Irradiância Média (W/m²)': '{:.2f}'}))
-
-st.title('Top 10 Menores Irradiâncias Médias')
-st.table(df_top_10_lowest.style.format({'Irradiância Média (W/m²)': '{:.2f}'}))
+    disponibilidade()
