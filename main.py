108c680a304fcb11ffbc0a1a5ed129203a3586df
PabloNvnes <pablonunes1500@gmail.com>
2025-01-19 22:39:13 -0300

    teste

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pysolar.solar import get_altitude
 from pysolar.radiation import get_radiation_direct
 import pandas as pd
 import plotly.express as px
+import random
+import openpyxl
 
 ###############
 ##  Funções  ## 
@@ -25,50 +27,28 @@ def calculate_inverter_performance(irradiance, efficiency=0.20):
 
 def irradiancia():
 
-    # Lista de cidades representativas de cada estado do Brasil com suas coordenadas
-    cities = [
-        {"state": "São Paulo", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
-        {"state": "Rio de Janeiro", "lat": -22.9068, "lon": -43.1729, "timezone": "America/Sao_Paulo"},
-        {"state": "Minas Gerais", "lat": -19.9167, "lon": -43.9345, "timezone": "America/Sao_Paulo"},
-        {"state": "Distrito Federal", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
-        {"state": "Bahia", "lat": -12.9714, "lon": -38.5014, "timezone": "America/Bahia"},
-        {"state": "Ceará", "lat": -3.7172, "lon": -38.5434, "timezone": "America/Fortaleza"},
-        {"state": "Paraná", "lat": -25.4284, "lon": -49.2733, "timezone": "America/Sao_Paulo"},
-        {"state": "Amazonas", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"},
-        {"state": "Pernambuco", "lat": -8.0476, "lon": -34.8770, "timezone": "America/Recife"},
-        {"state": "Rio Grande do Sul", "lat": -30.0346, "lon": -51.2177, "timezone": "America/Sao_Paulo"},
-        {"state": "Pará", "lat": -1.4558, "lon": -48.4902, "timezone": "America/Belem"},
-        {"state": "Goiás", "lat": -16.6869, "lon": -49.2648, "timezone": "America/Sao_Paulo"},
-        {"state": "Maranhão", "lat": -2.5307, "lon": -44.3068, "timezone": "America/Fortaleza"},
-        {"state": "Alagoas", "lat": -9.6658, "lon": -35.7350, "timezone": "America/Maceio"},
-        {"state": "Rio Grande do Norte", "lat": -5.7945, "lon": -35.2110, "timezone": "America/Fortaleza"},
-        {"state": "Piauí", "lat": -5.0919, "lon": -42.8034, "timezone": "America/Fortaleza"},
-        {"state": "Paraíba", "lat": -7.1195, "lon": -34.8450, "timezone": "America/Fortaleza"},
-        {"state": "Sergipe", "lat": -10.9472, "lon": -37.0731, "timezone": "America/Maceio"},
-        {"state": "Mato Grosso do Sul", "lat": -20.4697, "lon": -54.6201, "timezone": "America/Campo_Grande"},
-        {"state": "Mato Grosso", "lat": -15.6014, "lon": -56.0979, "timezone": "America/Cuiaba"},
-        {"state": "Tocantins", "lat": -10.2491, "lon": -48.3243, "timezone": "America/Araguaina"},
-        {"state": "Roraima", "lat": 2.8235, "lon": -60.6758, "timezone": "America/Boa_Vista"},
-        {"state": "Amapá", "lat": 0.0340, "lon": -51.0694, "timezone": "America/Belem"},
-        {"state": "Rondônia", "lat": -8.7612, "lon": -63.9039, "timezone": "America/Porto_Velho"},
-        {"state": "Acre", "lat": -9.9747, "lon": -67.8243, "timezone": "America/Rio_Branco"}
+    # Lista de usinas fictícias com suas coordenadas
+    plants = [
+        {"plant": "Usina 1", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
+        {"plant": "Usina 2", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
+        {"plant": "Usina 3", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"}
     ]
 
-    # Obter dados de irradiância solar para as últimas 24 horas para cada cidade
-    irradiance_data = {city["state"]: [] for city in cities}
+    # Obter dados de irradiância solar para as últimas 24 horas para cada usina
+    irradiance_data = {plant["plant"]: [] for plant in plants}
     time_data = []
     current_time = datetime.now(pytz.utc)
     for hour in range(24):
         time_point = current_time - timedelta(hours=hour)
         time_data.append(time_point)
-        for city in cities:
-            timezone = pytz.timezone(city["timezone"])
+        for plant in plants:
+            timezone = pytz.timezone(plant["timezone"])
             local_time = time_point.astimezone(timezone)
-            irradiance = get_solar_irradiance(city["lat"], city["lon"], local_time)
-            irradiance_data[city["state"]].append(irradiance)
+            irradiance = get_solar_irradiance(plant["lat"], plant["lon"], local_time)
+            irradiance_data[plant["plant"]].append(irradiance)
 
-    # Calcular a irradiância solar total para cada estado
-    total_irradiance = {state: sum(irradiance_data[state]) for state in irradiance_data}
+    # Calcular a irradiância solar total para cada usina
+    total_irradiance = {plant: sum(irradiance_data[plant]) for plant in irradiance_data}
 
     # Criar DataFrame para facilitar a manipulação dos dados
     df = pd.DataFrame(irradiance_data, index=time_data)
@@ -77,66 +57,47 @@ def irradiancia():
     df = df.iloc[::-1]
 
     # Criar DataFrame para a soma total da irradiância
-    df_total_irradiance = pd.DataFrame(list(total_irradiance.items()), columns=['Estado', 'Irradiância Total (W/m²)'])
+    df_total_irradiance = pd.DataFrame(list(total_irradiance.items()), columns=['Usina', 'Irradiância Total (W/m²)'])
 
     # Criar gráfico de barras com Plotly Express
-    fig_bar = px.bar(df_total_irradiance, x='Estado', y='Irradiância Total (W/m²)', title='Irradiância Solar Total por Estado nas Últimas 24 Horas')
-    fig_bar.update_layout(xaxis_title='Estado', yaxis_title='Irradiância Total (W/m²)', legend_title_text='Estado')
+    fig_bar = px.bar(df_total_irradiance, x='Usina', y='Irradiância Total (W/m²)', title='Irradiância Solar Total por Usina nas Últimas 24 Horas')
+    fig_bar.update_layout(xaxis_title='Usina', yaxis_title='Irradiância Total (W/m²)', legend_title_text='Usina')
 
     # Exibir o gráfico de barras no Streamlit
-    st.title('Irradiância Solar Total por Estado nas Últimas 24 Horas')
+    st.title('Irradiância Solar Total por Usina nas Últimas 24 Horas')
     st.plotly_chart(fig_bar, use_container_width=True)
 
     # Criar gráfico de linhas com Plotly Express
     fig_line = px.line(df, x=df.index, y=df.columns, labels={'value': 'Irradiância Solar (W/m²)', 'index': 'Hora'}, title='Irradiância Solar nas Últimas 24 Horas')
-    fig_line.update_layout(xaxis_title='Hora', yaxis_title='Irradiância Solar (W/m²)', legend_title_text='Estado')
+    fig_line.update_layout(xaxis_title='Hora', yaxis_title='Irradiância Solar (W/m²)', legend_title_text='Usina')
 
     # Exibir o gráfico de linhas no Streamlit
     st.plotly_chart(fig_line, use_container_width=True)
 
 def performance():
-    # Lista de cidades representativas de cada estado do Brasil com suas coordenadas
-    cities = [
-        {"state": "São Paulo", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
-        {"state": "Rio de Janeiro", "lat": -22.9068, "lon": -43.1729, "timezone": "America/Sao_Paulo"},
-        {"state": "Minas Gerais", "lat": -19.9167, "lon": -43.9345, "timezone": "America/Sao_Paulo"},
-        {"state": "Distrito Federal", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
-        {"state": "Bahia", "lat": -12.9714, "lon": -38.5014, "timezone": "America/Bahia"},
-        {"state": "Ceará", "lat": -3.7172, "lon": -38.5434, "timezone": "America/Fortaleza"},
-        {"state": "Paraná", "lat": -25.4284, "lon": -49.2733, "timezone": "America/Sao_Paulo"},
-        {"state": "Amazonas", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"},
-        {"state": "Pernambuco", "lat": -8.0476, "lon": -34.8770, "timezone": "America/Recife"},
-        {"state": "Rio Grande do Sul", "lat": -30.0346, "lon": -51.2177, "timezone": "America/Sao_Paulo"},
-        {"state": "Pará", "lat": -1.4558, "lon": -48.4902, "timezone": "America/Belem"},
-        {"state": "Goiás", "lat": -16.6869, "lon": -49.2648, "timezone": "America/Sao_Paulo"},
-        {"state": "Maranhão", "lat": -2.5307, "lon": -44.3068, "timezone": "America/Fortaleza"},
-        {"state": "Alagoas", "lat": -9.6658, "lon": -35.7350, "timezone": "America/Maceio"},
-        {"state": "Rio Grande do Norte", "lat": -5.7945, "lon": -35.2110, "timezone": "America/Fortaleza"},
-        {"state": "Piauí", "lat": -5.0919, "lon": -42.8034, "timezone": "America/Fortaleza"},
-        {"state": "Paraíba", "lat": -7.1195, "lon": -34.8450, "timezone": "America/Fortaleza"},
-        {"state": "Sergipe", "lat": -10.9472, "lon": -37.0731, "timezone": "America/Maceio"},
-        {"state": "Mato Grosso do Sul", "lat": -20.4697, "lon": -54.6201, "timezone": "America/Campo_Grande"},
-        {"state": "Mato Grosso", "lat": -15.6014, "lon": -56.0979, "timezone": "America/Cuiaba"},
-        {"state": "Tocantins", "lat": -10.2491, "lon": -48.3243, "timezone": "America/Araguaina"},
-        {"state": "Roraima", "lat": 2.8235, "lon": -60.6758, "timezone": "America/Boa_Vista"},
-        {"state": "Amapá", "lat": 0.0340, "lon": -51.0694, "timezone": "America/Belem"},
-        {"state": "Rondônia", "lat": -8.7612, "lon": -63.9039, "timezone": "America/Porto_Velho"},
-        {"state": "Acre", "lat": -9.9747, "lon": -67.8243, "timezone": "America/Rio_Branco"}
+    # Lista de usinas fictícias com suas coordenadas
+    plants = [
+        {"plant": "Usina 1", "lat": -23.5505, "lon": -46.6333, "timezone": "America/Sao_Paulo"},
+        {"plant": "Usina 2", "lat": -15.7942, "lon": -47.8822, "timezone": "America/Sao_Paulo"},
+        {"plant": "Usina 3", "lat": -3.1190, "lon": -60.0217, "timezone": "America/Manaus"}
     ]
 
-    # Obter dados de irradiância solar para as últimas 24 horas para cada cidade a cada 5 minutos
-    performance_data = {city["state"]: [] for city in cities}
+    # Obter dados de irradiância solar para as últimas 24 horas para cada usina a cada 5 minutos
+    performance_data = {plant["plant"]: [] for plant in plants}
     time_data = []
     current_time = datetime.now(pytz.utc)
     for minute in range(0, 24 * 60, 5):
         time_point = current_time - timedelta(minutes=minute)
         time_data.append(time_point)
-        for city in cities:
-            timezone = pytz.timezone(city["timezone"])
+        for plant in plants:
+            timezone = pytz.timezone(plant["timezone"])
             local_time = time_point.astimezone(timezone)
-            irradiance = get_solar_irradiance(city["lat"], city["lon"], local_time)
+            irradiance = get_solar_irradiance(plant["lat"], plant["lon"], local_time)
             performance = calculate_inverter_performance(irradiance)
-            performance_data[city["state"]].append(performance)
+            # Simular problemas nos inversores
+            if random.random() < 0.05:  # 5% de chance de problema
+                performance *= random.uniform(0.5, 0.9)  # Reduzir performance entre 10% e 50%
+            performance_data[plant["plant"]].append(performance)
 
     # Criar DataFrame para facilitar a manipulação dos dados
     df_performance = pd.DataFrame(performance_data, index=time_data)
@@ -146,45 +107,78 @@ def performance():
 
     # Criar gráfico de linhas com Plotly Express
     fig_performance = px.line(df_performance, x=df_performance.index, y=df_performance.columns, labels={'value': 'Performance do Inversor (W)', 'index': 'Hora'}, title='Performance do Inversor Solar a Cada 5 Minutos')
-    fig_performance.update_layout(xaxis_title='Hora', yaxis_title='Performance do Inversor (W)', legend_title_text='Estado')
+    fig_performance.update_layout(xaxis_title='Hora', yaxis_title='Performance do Inversor (W)', legend_title_text='Usina')
 
     # Exibir o gráfico de linhas no Streamlit
     st.title('Performance do Inversor Solar a Cada 5 Minutos')
     st.plotly_chart(fig_performance, use_container_width=True)
 
-def indicadores_realtime():
-    return
+def save_to_excel(data):
+    df = pd.DataFrame(data)
+    with pd.ExcelWriter('motivos.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+        df.to_excel(writer, index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
+
+def detalhamento():
+    st.title('Detalhamento de Motivos')
+    usina = st.selectbox('Usina', ['Usina 1', 'Usina 2', 'Usina 3'])
+    tipo_equipamento = st.selectbox('Tipo de Equipamento', ['Inversor', 'SB'])
+    identificador_equipamento = st.selectbox('Identificador do Equipamento', [
+        '331', '332', '333',  # Usina 1
+        '431', '432', '433',  # Usina 2
+        '531', '532', '533'   # Usina 3
+    ])
+    motivo = st.selectbox('Motivo', ['Bom', 'Ruim'])
+    motivo_detalhado = st.text_area('Motivo Detalhado')
+    data = st.date_input('Data')
+
+    if st.button('Salvar'):
+        data = {
+            'Usina': [usina],
+            'Tipo Equipamento': [tipo_equipamento],
+            'Identificador Equipamento': [identificador_equipamento],
+            'Motivo': [motivo],
+            'Motivo Detalhado': [motivo_detalhado],
+            'Data': [data]
+        }
+        save_to_excel(data)
+        st.success('Motivo salvo com sucesso!')
 
 #####################
 ## Streamlit Geral ##
 #####################
 
 # Adicionar uma barra lateral
-add_sidebar = st.sidebar.selectbox('Análises', ('Irradiância Solar', 'Performance e disponibilidade das Usinas', 'Indicadores Real Time'))
+add_sidebar = st.sidebar.selectbox('Análises', ('Real Time', 'Resumo Usina', 'Detalhamento Inversores', 'Detalhamento SBs'))
 
 #################
-## Irradiância ##
+## Real Time ##
 #################
 
-if add_sidebar == 'Irradiância Solar':
-    st.title('Irradiância Solar')
+if add_sidebar == 'Real Time':
+    st.title('Indicadores Real Time')
+    # Adicione aqui o código para os indicadores em tempo real
 
+#################
+## Resumo Usina ##
+#################
+
+if add_sidebar == 'Resumo Usina':
+    st.title('Resumo Usina')
     irradiancia()
 
 #############################
-## Performance dos Estados ##
+## Detalhamento Inversores ##
 #############################
 
-if add_sidebar == 'Performance e disponibilidade das Usinas':
-    st.title('Performance e disponibilidade das Usinas')
-
+if add_sidebar == 'Detalhamento Inversores':
+    st.title('Detalhamento Inversores')
     performance()
+    detalhamento()
 
-################################
-## Disponibilidade de Energia ##
-################################
+#############################
+## Detalhamento SBs ##
+#############################
 
-if add_sidebar == 'Indicadores Real Time':
-    st.title('Indicadores Real Time')
-
-    indicadores_realtime()
+if add_sidebar == 'Detalhamento SBs':
+    st.title('Detalhamento SBs')
+    detalhamento()
